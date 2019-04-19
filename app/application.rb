13808b45ac957bd6d5a2329fd0ff88191daaa439
Ivan Szebenszki <szib77@gmail.com>
2019-04-19 07:44:04 +0100

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,20 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+
+    arr = []
+    3.times do
+      num = Kernel.rand(1..20)
+      arr << num
+      resp.write "#{num}\n"
+    end
+
+    if arr.uniq.length == 1
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+    end
+
     resp.finish
   end
 
